6c726c79a89f5e7d8fcf5233a6217245e4e293f0
kumarnishu10490 <kumarnishu10490@gmail.com>
2026-04-26 00:31:35 +0530

    UI: Removed QuickActionToolbar based on user feedback.

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
 import WhatsAppButton from "./components/WhatsAppButton";
 import SocialProofPopup from "./components/SocialProofPopup";
-import QuickActionToolbar from "./components/QuickActionToolbar";
 import AISkinAnalysisPage from "./pages/AISkinAnalysisPage";
 import AIHairstylePage from "./pages/AIHairstylePage";
 import AIBeautyConsultantPage from "./pages/AIBeautyConsultantPage";
@@ -63,7 +62,6 @@ const App = () =>
         </AnimatePresence>
         <WhatsAppButton />
         <SocialProofPopup />
-        <QuickActionToolbar />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>;
